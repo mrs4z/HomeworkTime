43e74f34b39eb6faf3c399c5f1b1b0c034d87e9b
mrs4z <mrs4z@icloud.com>
2021-09-30 18:19:24 +0300

    Initial Commit

---
+++ HomeworkTime/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeworkTime
+//
+//  Created by Александр Горденко on 30.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
